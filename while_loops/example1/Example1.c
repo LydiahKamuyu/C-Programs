41e10220c7ca449fc0dc1fdc8b0e0892d8c442bc
LydiahKamuyu <kamuyulydia@gmail.com>
2023-03-01 13:48:35 +0300

    Building a guessing game with limit of 4 guesses

--- while_loops/example1/Example1.c
+++ while_loops/example1/Example1.c
@@ -1,16 +1,30 @@
-// building a guessing game
+// building a guessing game with only 4 chances to guess
 #include <stdio.h>
 
 int main() {
-     int secretno = 5;
+     int secretno = 10;
      int guess;
+     int guesscount = 0; //the times user tries to guess secretno initial is 0.
+     int guesslimit = 4; //no of times user can guess
+     int outofguesses = 0; //tells whether user has any guesses left
      
      
-     while(guess != secretno){
-         printf("enter a number");
-         scanf(" %d", &guess);
+     while(guess != secretno && outofguesses == 0){
+         if(guesscount < guesslimit){
+             printf("enter a number\n");
+             scanf(" %d", &guess);
+             guesscount++; 
+             
+         }else
+         {
+             outofguesses = 1; //breaks out of loop when outofguesses is no longer equal to 0. Run out of guesses
+         }
      }
-     printf("You win");
-
-    return 0;
+     if(outofguesses == 1)
+     {
+         printf("out of guesses\n");
+     }else
+     {
+          printf("Hurry, You've Won\n");
+     }
 }
